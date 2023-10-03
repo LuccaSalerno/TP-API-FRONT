b9158697cc0c66f57b9689428a7cbbca300f7a4a
KatherineDG <katherinedomeqg@gmail.com>
2023-10-03 12:19:21 -0300

    v2 - login

--- src/Login.js
+++ src/Login.js
@@ -1,18 +1,19 @@
 import React, { useState } from 'react';
 import './Login.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { useHistory } from 'react-router-dom';
+import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
+import { Redirect } from 'react-router-dom';
 
 //Dentro de la función puedo hacer lógica
 
+
+//Iniciar Sesion
+//Registrarse
 function LoginComponente(){
 
     //Control para cambiar de pantalla
-    const [Logeado, setLogeado] = useState(false);
-    const history = useHistory();
+    const [logeado, setLogeado] = useState(false);
 
-    //Iniciar Sesion
-    //Registrarse
     //Persona
     const [persona, crearPersona] = useState({nombre:'', password:'', documento:''});
 
@@ -26,18 +27,18 @@ function LoginComponente(){
             const personaEncontrada = await respuesta.json();
             if (personaEncontrada) {
               console.log('La persona existe');
-              return true;  // Devuelve true si la persona se encontró
+              return true;
             } else {
               console.log('La persona no existe');
-              return false;  // Devuelve false si la persona no se encontró
+              return false;
             }
           } else {
-            console.log('NO ESTA');
-            return false;  // Devuelve false si hubo un problema con la solicitud
+            console.log('Respuesta no existosa');
+            return false;
           }
         } catch (error) {
           console.log('Error al buscar la persona o no se encuentra registrado');
-          return false;  // Devuelve false si hubo un error en la solicitud
+          return false;
         }
       };
       
@@ -53,26 +54,21 @@ function LoginComponente(){
               body: JSON.stringify(persona),
             });
             if (respuesta.ok) {
-                // Registro exitoso
                 console.log('Usuario registrado con éxito');
             } else {
-                // Manejar errores de registro
                 console.error('Error al registrar usuario');
             }
-
             } catch (error) {
               console.log('Error de red');
             }
     };
 
-    //Control de lo que se está escribiendo tenga un estado de entrada
-    //se crea la persona, "e" es el estado de la persona que tiene valores.
+    //Guarda a la persona con sus datos
     const manejarCambioEntrada = (e) => {
         crearPersona({ ...persona, [e.target.name]: e.target.value });
     };
 
-    
-    //Verifica que al menos que se ingresó algo en el docmuento
+    //Verifica que al menos que se ingresó algo en el "docmuento"
     const verificarInputs = () =>{
       //const nombre = document.getElementById('nombre').value;
       //const contrasenia = document.getElementById('password').value;
@@ -83,11 +79,10 @@ function LoginComponente(){
       else{
         return false
       }
-
     }
 
     async function Registrar(){
-      if (verificarInputs == true){
+      if (verificarInputs() === true){
         const resultadoBusqueda = await buscarPersona();
         console.log(resultadoBusqueda);  // true o false
         if (resultadoBusqueda == false){
@@ -104,48 +99,45 @@ function LoginComponente(){
     }
 
     async function IniciarSesion(){
-      if (verificarInputs == true){
-        const resultadoBusqueda = await buscarPersona();
-        console.log(resultadoBusqueda);  // true o false
-        if (resultadoBusqueda == true){
-            alert('Entraste a la plataforma')
-            setLogeado(true);
-            //Puede cambiar a la siguiente pantalla
-            //setLogeado(true);
-            //Redirigo al usuario a la sigueinte pantalla
-            history.push('/inicio');
-         }
-         else{
-            alert('No existe el usuario. Registrese')
-         }
-      }
-      else{
-        alert('Ingrese algo')
-      }
-        
-      
-        
+        if (verificarInputs() === true){
+            const resultadoBusqueda = await buscarPersona();
+            console.log(resultadoBusqueda);  // true o false
+            if (resultadoBusqueda == true){
+                alert('Entraste a la plataforma')
+                //Permiso para poder ir a la siguiente pantalla
+                setLogeado(true);
+             }
+             else{
+                alert('No existe el usuario. Registrese')
+             }
+        }
+        else{
+            alert('Complete con sus datos')
+        }    
     }
 
         
     return(
-
         <div className='Login'>
-            
+          {!logeado ? (
+            //Usuario no autenticado, muestro el formulario Login
             <div className='contenedor-datos'>
-                <h1 className='bienvenido'>¡Bienvenido/a!</h1>
-
-              <form>
-                <input className='globo' type="text" placeholder="Nombre" name="nombre" id='nombre' value={persona.nombre} onChange={manejarCambioEntrada} required/>
-                <input className='globo' type="password" placeholder="Contraseña" name="password" id='password' value={persona.password} onChange={manejarCambioEntrada} required/>
-                <input className='globo' type="text" placeholder="Documento" name="documento" id='documento' value={persona.documento} onChange={manejarCambioEntrada} required/>
-                <button className='globo-boton' onClick={IniciarSesion}>Iniciar Sesion </button>
-                <button className='globo-boton' onClick={Registrar}>Registrarse</button>
-              </form>
-                
-                
-                
+              <h1 className='bienvenido'>¡Bienvenido/a!</h1>
+                <form>
+                  <input className='globo' type="text" placeholder="Nombre" name="nombre" id='nombre' value={persona.nombre} onChange={manejarCambioEntrada} required/>
+                  <input className='globo' type="password" placeholder="Contraseña" name="password" id='password' value={persona.password} onChange={manejarCambioEntrada} required/>
+                  <input className='globo' type="text" placeholder="Documento" name="documento" id='documento' value={persona.documento} onChange={manejarCambioEntrada} required/>
+                  <button className='globo-boton' type='submit' onClick={() => IniciarSesion(persona)}>Iniciar Sesion </button>
+                  <button className='globo-boton' type='submit' onClick={() => Registrar()}>Registrarse</button>
+                </form>
             </div>
+          ) : (
+            // Si el usuario está autenticado, pasamos la información de persona a InicioComponente
+            <Redirect to={{
+              pathname: "/inicio",
+              state: { persona: persona }
+            }} />
+          )};  
             
         </div>
     );
--- src/Reclamo.js
+++ src/Reclamo.js
@@ -1,14 +1,23 @@
 import React, { useState } from 'react';
-import './Inicio.css';
+import './Reclamo.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { useHistory } from 'react-router-dom';
+import { useHistory} from 'react-router-dom';
+import { Redirect } from 'react-router-dom';
 
 
 
-function InicioComponente(){
+function InicioComponente(props){
+
+  const persona = props.location.state.persona;
+
+
   const history = useHistory();
 
-  const [edificio, crearPersona] = useState({codigo:''});
+  const [edificio, crearEdificio] = useState({codigo:''});
+
+  const manejarCambioEntrada = (e) => {
+    crearEdificio({ ...edificio, [e.target.name]: e.target.value });
+  };
 
   const buscarEdificio = async () => {
     try {
@@ -33,8 +42,27 @@ function InicioComponente(){
     }
   };
 
+
   return(
-      <h1>ESTA ES LA PANTALLA DE INCIO</h1>
+      <div className='PantallaReclamo'>
+
+        <div className='contenedor-datos'>
+          <h1 className='bienvenido'>¡Bienvenido/a! {persona.nombre}</h1>
+
+          <form>
+            
+            <input className='globo' type='number' placeholder="Codigo del Edificio" name="codigo" id='codigo' value={edificio.codigo} onChange={manejarCambioEntrada} required/>
+                
+            <button className='globo-boton' type='submit' onClick={() => buscarEdificio()}> Enviar Reclamo </button>
+        
+          </form>
+
+
+        </div>
+        
+
+      </div>
+
 
   )
 };
