2072fa6a7c3411f931457158393818fa12e718c3
Francisco Csik <francsik23@gmail.com>
2023-10-08 17:09:18 -0300

    .

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 
 import LoginComponente from './Login';
 import InicioComponente from './Reclamo';
+import Edificios from './Componentes/Edificios';
 
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route exact path='/login' element={<LoginComponente/>} />
         <Route exact path='/inicio' element={<InicioComponente/>} />
+        <Route exact path='/edificios' element={<Edificios/>} />
       </Routes>
     </Router>
   );
---
+++ src/Componentes/Edificios.js
@@ -0,0 +1,14 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function Edificios(){
+
+
+    return(
+        <div>
+            <h1>Edificios</h1>
+        </div>
+    )
+
+};
+
+export default Edificios;
